59fded5fedc56125a4c7873ad171abd2539fd40f
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-06-17 11:50:33 -0400

    deposit function started

--- project.js
+++ project.js
@@ -1,7 +1,14 @@
 // 1. Deposit some money
-// 2. Number of lines to bet
-// 3. Collect a bet amount
+// 2. Number of lines user wants to bet
+// 3. Collect amount of bet from user
 // 4. Spin slots
 // 5. Did they win
-// 6. Pay user winnings or take better
+// 6. Pay user winnings or take bet
 // 7. Play again
+const prompt = require('prompt-sync')();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
