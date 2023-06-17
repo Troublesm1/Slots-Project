824cf058b13d173d1b320e1affdd52d8d9a275a4
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-06-17 12:02:20 -0400

    Not a number added to deposit func

--- project.js
+++ project.js
@@ -9,6 +9,11 @@ const prompt = require('prompt-sync')();
 
 const deposit = () => {
     const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, please try again.");
+    }
 };
 
 deposit();
