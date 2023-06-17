e7fce7437ecf1229b153740d6d70622adaeaa143
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-06-17 11:44:28 -0400

    Outline of project

--- project.js
+++ project.js
@@ -1,1 +1,7 @@
-// 1.
+// 1. Deposit some money
+// 2. Number of lines to bet
+// 3. Collect a bet amount
+// 4. Spin slots
+// 5. Did they win
+// 6. Pay user winnings or take better
+// 7. Play again
