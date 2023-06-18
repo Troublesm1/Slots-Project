537baf5584d4e490d4009253694ab054f2ceef3a
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-06-18 11:05:05 -0400

    Symbols map added to array

--- project.js
+++ project.js
@@ -11,16 +11,16 @@ const prompt = require('prompt-sync')();
 const ROWS = 3;
 const COLS = 3;
 const SYMBOLS_COUNT = {
-    "A": 2,
-    "B": 4,
-    "C": 6,
-    "D": 8
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
 }
  const SYMBOL_VALUES = {
-    "A": 5,
-    "B": 4,
-    "C": 3,
-    "D": 2
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
  }
 
 const deposit = () => {
@@ -61,7 +61,17 @@ const getBet = (balance, lines) => {
             return numberBet;
         }
     }
-}
+};
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+    
+};
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
